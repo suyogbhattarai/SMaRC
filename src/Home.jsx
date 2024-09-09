c22c1fd30c3f31c72ec1c1c6d451a4899ee55e38
Aashutosh Dahal <165886572+AashutoshDL@users.noreply.github.com>
2024-09-09 15:09:22 +0545

    navbar changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,19 +20,23 @@ function Home() {
 
   return (
     <>
-      <div className="navbar-container">
-        <div className="logo">
-          <a href="./"><img src="./smarcLOGO.png" alt="Logo" /></a>
-        </div>
-        <div className="navbar">
-          <ul>
-            <li><a href="/">Home</a></li>
-            {/* <li><a href="/?redirect=navbar&scrollPosition=3.013384556516312">AssistedVision</a></li> */}
-            <li><a>Products</a></li>
-            <li><a href="/?redirect=navbarRobotics&scrollPosition=6.113384556516312">Skill Museum</a></li>
-          </ul>
-        </div>
+    <div className="navbar-container">
+
+      <div className="navbar-card">
+      <div className="logo">
+        <a href="./">
+          <img src="./smarcLOGO.png" alt="Logo" />
+        </a>
       </div>
+      <div className="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a>Products</a></li>
+          <li><a href="/?redirect=navbarRobotics&scrollPosition=6.113384556516312">Skill Museum</a></li>
+        </ul>
+      </div>
+    </div>
+    </div>
       <div className="body-container">
         <div className="text">
           <h1 className="smarc-text">
